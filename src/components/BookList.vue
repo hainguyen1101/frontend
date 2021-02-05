2c4ffff24fbc545ea93a2540b3f28e83b203196d
Hai Nguyen <nguyentrunghai11011992@gmail.com>
2021-02-04 20:09:15 -0800

    update souce code

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,12 +1,48 @@
 <template>
     <v-container>
-        <h3 class="text-left">Book List</h3>
         <v-row>
             <v-col>
-                <v-btn class="float-right" color="" @click="onAddClick()">
+                <h3 class="text-left">Book List</h3>
+            </v-col>
+        </v-row>
+        <v-row>
+
+            <v-col cols="4">
+                <v-select class="text-left" v-model="select" :items="items" label="Search condition" dense outlined
+                    v-on:change="changeSearchCondition">
+                </v-select>
+            </v-col>
+            <v-col cols="4" v-if="select == 'Book name'">
+                <v-text-field class="search-field" v-model="searchBookName" label="Book Name"></v-text-field>
+            </v-col>
+            <v-col cols="4" v-if="select == 'Publisher'">
+                <v-text-field class="search-field" v-model="searchPublisher" label="Publisher"></v-text-field>
+            </v-col>
+            <v-col cols="2" v-if="select == 'Price'">
+                <v-text-field width="60" class="search-field" v-model="searchPriceFrom" label="From"></v-text-field>
+
+            </v-col>
+            <v-col cols="2" v-if="select == 'Price'">
+                <v-text-field width="60" class="search-field" v-model="searchPriceTo" label="To"></v-text-field>
+            </v-col>
+            <v-col cols="4" v-if="select == ''||!select == 'Book name'||!select == 'Publisher'||!select == 'Price'">
+
+            </v-col>
+            <v-col cols="4">
+
+                <v-btn class="float-right" color="primary" @click="onSearchClick()">
+                    Search
+                </v-btn>
+            </v-col>
+
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-btn class="float-right" color="primary" @click="onAddClick()">
                     Add new book
                 </v-btn>
             </v-col>
+
         </v-row>
         <v-row>
             <v-col>
@@ -29,7 +65,8 @@
                 </v-data-table>
             </v-col>
         </v-row>
-        <CreateBookDialog v-model="showScheduleForm" @eventname="updateparent" :title="titleDialog"></CreateBookDialog>
+        <CreateBookDialog ref="bookDialogComponent" v-model="showScheduleForm" @eventname="updateparent"
+            :title="titleDialog" :editBookObject="editBookObject" :mode="mode"></CreateBookDialog>
     </v-container>
 
 
@@ -50,32 +87,49 @@
                 headers: [{
                     text: 'Image',
                     value: 'image',
+                    sortable: false,
+                    width: "200px"
                 }, {
                     text: 'Book name',
                     align: 'start',
                     sortable: true,
                     value: 'name',
+                    width: "150px"
                 }, {
                     text: 'Publisher',
                     align: 'start',
                     sortable: true,
                     value: 'publisher',
+                    width: "150px"
                 }, {
                     text: 'Price',
                     align: 'start',
                     sortable: true,
                     value: 'price',
+                    width: "100px"
                 }, {
                     text: 'Publish Year',
                     align: 'start',
                     sortable: true,
                     value: 'publish_year',
+                    width: "200px"
                 },
                 {
                     text: 'Action',
                     value: 'action',
+                    sortable: false,
+                    width: "150px"
                 }],
-                titleDialog: "aaa"
+                titleDialog: "",
+                editBookObject: null,
+                mode: 0,
+                items: ['', 'Book name', 'Publisher', 'Price', 'Publish Year'],
+                select: '',
+                searchBookName: '',
+                searchPublisher: '',
+                listCondition: [],
+                searchPriceTo: '',
+                searchPriceFrom: ''
 
             }
         },
@@ -105,20 +159,51 @@
             },
             onEditClick(item) {
                 console.log(item);
+                this.editBookObject = item;
+                this.showScheduleForm = true;
                 this.titleDialog = "Edit book";
+                this.$refs.bookDialogComponent.setEditForm();
+                this.mode = 1;
             },
             onAddClick() {
                 this.showScheduleForm = true;
                 this.titleDialog = "Create new book";
-                // this.$refs.form.reset();
+                this.editBookObject = null;
+                // this.$refs.bookDialogComponent.resetForm();
+                this.mode = 0;
             },
             updateparent(result) {
                 this.booklist = result;
+            },
+            changeSearchCondition() {
+                // alert(this.select);
+            },
+            onSearchClick() {
+                let data = {
+                    field: this.select,
+                    name: ''
+                }
+                if (this.select == 'Book name') {
+                    data.name = this.searchBookName
+                }
+
+                if (this.select == 'Publisher') {
+                    data.name = this.searchPublisher
+                }
+                if (this.select == 'Price') {
+                    data.from = this.searchPriceFrom
+                    data.to = this.searchPriceTo
+                }
+                axios.post(" http://localhost:8082/book/search", data).then(response => {
+                    this.booklist = response.data;
+                });
             }
         }
     }
 </script>
 
 <style>
-
+    div.search-field {
+        margin-top: -7px;
+    }
 </style>
--- src/components/CreateBookDialog.vue
+++ src/components/CreateBookDialog.vue
@@ -10,31 +10,36 @@
                         <v-form ref="form" v-model="valid">
                             <v-row>
                                 <v-col cols="6">
-                                    <v-card outlined height="500px">
-                                        <v-img v-if="url" :src="url" contain height="500px"></v-img>
+                                    <v-card outlined height="400px">
+                                        <v-img v-if="url" :src="url" contain height="400px"></v-img>
+                                        <v-img v-if="this.editBookObject && !url"
+                                            v-bind:src="'data:image/jpeg;base64,'+this.editBookObject.image" contain
+                                            height="400px"></v-img>
                                     </v-card>
                                 </v-col>
                                 <v-col cols="6">
 
-                                        <v-text-field :rules="nameRules" v-model="bookName" label="Book Name*" required>
-                                        </v-text-field>
-                                        <v-text-field :rules="publisherRules" v-model="publisher" label="Publisher*"
-                                            required></v-text-field>
-                                        <v-text-field :rules="priceRules" v-model="price" label="Price*" required>
-                                        </v-text-field>
-                                        <v-text-field :rules="yearRules" v-model="publishYear" label="Publish Year*"
-                                            required></v-text-field>
-                                        <v-file-input :rules="fileRules" accept="image/*" truncate-length="15"
-                                            v-on:change="chooseFile"></v-file-input>
-                                        <v-card-actions>
-                                            <v-spacer></v-spacer>
-                                            <v-btn color="depressed" @click="show=false">
-                                                Close
-                                            </v-btn>
-                                            <v-btn :disabled="!valid" depressed color="primary" @click="save">
-                                                Save
-                                            </v-btn>
-                                        </v-card-actions>
+                                    <v-text-field :rules="nameRules" v-model="bookName" label="Book Name*" required>
+                                    </v-text-field>
+                                    <v-text-field :rules="publisherRules" v-model="publisher" label="Publisher*"
+                                        required></v-text-field>
+                                    <v-text-field :rules="priceRules" v-model="price" label="Price*" required>
+                                    </v-text-field>
+                                    <v-text-field :rules="yearRules" v-model="publishYear" label="Publish Year*"
+                                        required></v-text-field>
+                                    <v-file-input v-if="this.mode == 0" :rules="fileRules" accept="image/*"
+                                        truncate-length="15" v-on:change="chooseFile"></v-file-input>
+                                    <!-- <v-file-input v-else :rules="fileEditRules" accept="image/*" truncate-length="15"
+                                        v-on:change="chooseFile"></v-file-input> -->
+                                    <v-card-actions class="dialog-button">
+                                        <v-spacer></v-spacer>
+                                        <v-btn color="depressed" @click="close">
+                                            Close
+                                        </v-btn>
+                                        <v-btn :disabled="!valid" depressed color="primary" @click="save">
+                                            Save
+                                        </v-btn>
+                                    </v-card-actions>
                                 </v-col>
                             </v-row>
                         </v-form>
@@ -53,7 +58,9 @@
         props: {
             value: Boolean,
             bookList: Object,
-            title: String
+            title: String,
+            editBookObject: Object,
+            mode: Number
         },
         data: function () {
             return {
@@ -73,13 +80,17 @@
                 ],
                 priceRules: [v => !!v || 'Price is required',
                 v => /^\d+$/.test(v) || 'Price only accept numbers',
-                v => (v.length < 4) || 'Price must be less than 1000'],
+                v => (v && v.length < 4) || 'Price must be less than 1000'],
                 yearRules: [v => !!v || 'Year is required',
                 v => /^\d+$/.test(v) || 'Year only accept numbers',
                 v => /^(194[5-9]|19[5-9]\d|200\d|201[0-9])$/.test(v) || 'Year must be from 1945-2019'],
                 fileRules: [v => !!v || 'File is required',
                 v => /png|jpg|jpeg|gif|tif|tiff/i.test(v && v.name) || 'File must be image'],
-                url: null
+                fileEditRules: [
+                    v => /png|jpg|jpeg|gif|tif|tiff/i.test(v && v.name) || 'File must be image'],
+                url: null,
+                editObject: null,
+                file: null
 
             }
         },
@@ -95,29 +106,55 @@
         },
         created() {
         },
+        watch: {
+            editBookObject() {
+                if (this.editBookObject) {
+                    this.bookName = this.editBookObject.name;
+                    this.publisher = this.editBookObject.publisher;
+                    this.publishYear = this.editBookObject.publish_year;
+                    this.price = this.editBookObject.price;
+                }
+            }
+        },
         methods: {
-
             save: function () {
-                var bodyFormData = new FormData();
-                bodyFormData.append('name', this.bookName);
-                bodyFormData.append('publisher', this.publisher);
-                bodyFormData.append('price', this.price);
-                bodyFormData.append('publish_year', this.publishYear);
-                bodyFormData.append('image', this.imageFile);
-                // let data = {
-                //     name: this.bookName,
-                //     publisher: this.publisher,
-                //     price: this.price,
-                //     publish_year: this.publishYear,
-                //     image: 
-                // }
-                this.show = false;
-                axios.post("http://localhost:8082/book", bodyFormData).then(response => {
-                    axios.get("http://localhost:8082/book").then(response => {
-                        this.$emit('eventname', response.data);
+                if (this.mode == 0) {
+                    var bodyFormData = new FormData();
+                    bodyFormData.append('name', this.bookName);
+                    bodyFormData.append('publisher', this.publisher);
+                    bodyFormData.append('price', this.price);
+                    bodyFormData.append('publish_year', this.publishYear);
+                    bodyFormData.append('image', this.imageFile);
+                    // let data = {
+                    //     name: this.bookName,
+                    //     publisher: this.publisher,
+                    //     price: this.price,
+                    //     publish_year: this.publishYear,
+                    //     image: 
+                    // }
+                    this.show = false;
+                    axios.post("http://localhost:8082/book", bodyFormData).then(response => {
+                        axios.get("http://localhost:8082/book").then(response => {
+                            this.$emit('eventname', response.data);
+                        });
+                        this.resetForm();
                     });
-
-                });
+                } else {
+                    var bodyFormData = new FormData();
+                    bodyFormData.append('id', this.editBookObject.id);
+                    bodyFormData.append('name', this.bookName);
+                    bodyFormData.append('publisher', this.publisher);
+                    bodyFormData.append('price', this.price);
+                    bodyFormData.append('publish_year', this.publishYear);
+                    bodyFormData.append('image', this.imageFile);
+                    this.show = false;
+                    axios.put("http://localhost:8082/book", bodyFormData).then(response => {
+                        axios.get("http://localhost:8082/book").then(response => {
+                            this.$emit('eventname', response.data);
+                        });
+                        this.resetForm();
+                    });
+                }
             },
             chooseFile(e) {
                 if (e) {
@@ -127,8 +164,40 @@
                 } else {
                     this.url = null;
                 }
+            },
+            resetForm() {
+                // this.$refs.form.resetValidation();
+                // if (this.mode == 0) {
+                this.$refs.form.reset();
+                this.url = null;
+                // this.editBookObject = null;
+                // } else {
+                this.$refs.form.resetValidation();
+                // }
 
+                // this.editBookObject = null;
+            },
+            setEditForm() {
+                if (this.editBookObject) {
+                    this.bookName = this.editBookObject.name;
+                    this.publisher = this.editBookObject.publisher;
+                    this.publishYear = this.editBookObject.publish_year;
+                    this.price = this.editBookObject.price;
+                }
+
+            },
+            close() {
+                this.resetForm();
+                this.show = false;
             }
+
         }
     }
 </script>
+<style>
+    .dialog-button {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+    }
+</style>
